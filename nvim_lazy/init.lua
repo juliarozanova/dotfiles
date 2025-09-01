2b2335d95ad77656ba53dc9f016e3f7ff2b79ccc
Julia Rozanova <julia.rozanova@featurespace.co.uk>
2025-09-01 16:02:13 +0100

    neovim adjustment

--- nvim_lazy/init.lua
+++ nvim_lazy/init.lua
@@ -1,3 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd.colorscheme("dopamine")
+vim.cmd.colorscheme("ayu-light")
+
+require('lualine').setup({
+  options = {
+    theme = 'ayu',
+  },
+})
+
+vim.o.termguicolors = true
--- nvim_lazy/lua/plugins/colorscheme.lua
+++ nvim_lazy/lua/plugins/colorscheme.lua
@@ -1,22 +1,23 @@
 return {
-  -- add gruvbox
   { "juliarozanova/dopamine.nvim" },
+  -- { "juliarozanova/dopamine-light" },
   { "ellisonleao/gruvbox.nvim" },
   { "neanias/everforest-nvim" },
   { "sainnhe/everforest" },
   { "sainnhe/edge" },
   { "marko-cerovac/material.nvim" },
+  -- { "Shatur/neovim-ayu" },
+  { "nvimdev/zephyr-nvim" },
   { "catppuccin/nvim",            name = "catppuccin", lazy = false, priority = 1000 },
+
   {
-    'sainnhe/sonokai',
+    "dopamine-light",
+    dir = "/home/juliaroz/code/dopamine-light",
     lazy = false,
     priority = 1000,
     config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.sonokai_enable_italic = true
-      vim.g.sonokai_style = 'julia'
-      -- vim.cmd.colorscheme('sonokai')
-    end
+      -- Your config logic here
+      require("dopamine-light").setup()
+    end,
   }
 }
