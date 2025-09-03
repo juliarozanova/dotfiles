dc02238b8a491933dac45c2a001710263b92f3a8
Julia Rozanova <julia.rozanova@featurespace.co.uk>
2025-09-03 17:27:41 +0100

    obsidian config

--- nvim_lazy/lua/plugins/obsidian.lua
+++ nvim_lazy/lua/plugins/obsidian.lua
@@ -1,5 +1,5 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
@@ -44,12 +44,38 @@ return {
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
       template = "Template - Daily Notes.md"
     },
+
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
-      -- Set to false to disable completion.
+      -- Enables completion using nvim_cmp
       nvim_cmp = true,
+      -- Enables completion using blink.cmp
+      blink = false,
       -- Trigger completion at 2 chars.
       min_chars = 2,
+      -- Set to false to disable new note creation in the picker
+      create_new = true,
     },
+
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+      name = "snacks.pick",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
+
+    new_notes_location = "00-Inbox/",
   },
 }
